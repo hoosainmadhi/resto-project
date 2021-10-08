2747200ab20e3c58f6ba5e9e105d0876a88ed0a1
Hoosain Madhi <hoosain.madhi@gmail.com>
2021-10-08 11:01:11 +0200

    create add restuarant form

--- src/components/Add.vue
+++ src/components/Add.vue
@@ -1,6 +1,12 @@
 <template>
 <Header/>
     <h1>Hello Welcome to Add Restuarant Page</h1>
+     <form class="add">
+       <input type="text" name="name" placeholder="Enter Name" v-model="restuarant.name">
+       <input type="text" name="address" placeholder="Enter Address" v-model="restuarant.address">
+       <input type="text" name="contact" placeholder="Enter Contact" v-model="restuarant.contact">
+       <button type="button" v-on:click="addRestuarant">Add New Restuarant</button>
+     </form>
 </template>
 
 <script>
@@ -12,6 +18,21 @@ export default {
 
     components:{
         Header
+    },
+    data(){
+      return{
+        restuarant:{
+          name:'',
+          address:'',
+          contact:''
+
+        }
+      }
+    },
+    methods:{
+      addRestuarant(){
+        console.log(this.restuarant);
+      }
     },
 
   mounted()
@@ -25,3 +46,7 @@ export default {
   }
 }
 </script>
+
+<style>
+
+</style>
